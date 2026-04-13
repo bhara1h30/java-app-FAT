5611c11be43617caab9f3d661dfd7171f0cdb5c6
Bharathwaraj sethuraman <bharathwaraj7@gmail.com>
2026-04-13 12:49:07 +0530

    Add unit test for add method in App class
    Refactored testApp method to test add functionality.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,16 @@
 package com.example;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
+
     @Test
-    public void testApp() {
-        // A simple test that always passes to verify Jenkins setup
-        assertTrue(true);
+    public void testAdd() {
+        App app = new App();
+        int result = app.add(5, 10);
+        
+        // This checks if 5 + 10 actually equals 15
+        assertEquals("5 + 10 should be 15", 15, result);
     }
 }
